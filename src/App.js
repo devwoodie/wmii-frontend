cc0ff5c538ad3c0ad2e1bd823b9d721c05028768
devwoodie <ehddl453@naver.com>
2022-12-12 08:18:19 +0900

    feat: choice main color

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ const App = () => {
     return (
         <div className="App">
             <div className="box1 box"></div>
-            <div className="box2 box"></div>
-            <div className="box3 box"></div>
-            <div className="box4 box"></div>
-            <div className="box5 box"></div>
-            <div className="box6 box"></div>
             <Routes>
                 <Route path="/" element={} />
             </Routes>
