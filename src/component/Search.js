6e6ebfcbf05c68579e00eb805a78dd738e061a71
devwoodie <ehddl453@naver.com>
2022-12-13 15:19:43 +0900

    feat : search component publish fin

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,7 +1,18 @@
+import {BsSearch} from "react-icons/bs";
+
 const Search = () => {
     return(
         <div className="Search inner">
-
+            <div className="search-wrap">
+                <input type="text" className="search-box" placeholder="Search..."/>
+                <button type="submit" className="search-btn"><BsSearch /></button>
+            </div>
+            <ul className="hash-tag-wrap">
+                <li className="hash-tag">#올빼미</li>
+                <li className="hash-tag">#올빼미</li>
+                <li className="hash-tag">#올빼미</li>
+                <li className="hash-tag">#올빼미</li>
+            </ul>
         </div>
     )
 }
