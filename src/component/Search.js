7a7b89bb8b872f0fac1f6e85d7fccdbe9492346f
devwoodie <ehddl453@naver.com>
2022-12-20 13:32:37 +0900

    feat : axios setting

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,11 +1,47 @@
 import {BsSearch} from "react-icons/bs";
+import {useRef, useState} from "react";
+import axios from "axios";
 
 const Search = () => {
+
+    const searchText = useRef(null);
+    let [searchList, setSearchList] = useState([]);
+
+    const searchKeyword = () => {
+
+        const keyword = searchText.current.value;
+        const ID_KEY = '0BhdufNATIrhn8DtcoRu';
+        const SECRET_KEY = 'bEDLIjkryc';
+        const searchUrl = '/v1/search/movie.json';
+        //axios
+        if(keyword === ''){
+            alert('키워드를 입력해주세요');
+        }else{
+            axios.get(searchUrl,{
+                params: {
+                    query: keyword,
+                    display: 20
+                },
+                headers: {
+                    'X-Naver-Client-Id': ID_KEY,
+                    'X-Naver-Client-Secret': SECRET_KEY
+                }
+            }).then((response) => {
+                    const movieData = response.data;
+                    console.log(movieData)
+
+                }).catch((error) => {
+                console.log(error);
+            });
+        }
+    }
+
     return(
         <div className="Search inner">
+            <h2 className="wrap-tit"><span className="tit-icon"><BsSearch/></span> 영화 검색</h2>
             <div className="search-wrap">
-                <input type="text" className="search-box" placeholder="Search..."/>
-                <button type="submit" className="search-btn"><BsSearch /></button>
+                <input type="text" ref={searchText} className="search-box" placeholder="Search..."/>
+                <button type="submit" className="search-btn" onClick={searchKeyword}><BsSearch /></button>
             </div>
             <ul className="hash-tag-wrap">
                 <li className="hash-tag">#올빼미</li>
@@ -16,6 +52,14 @@ const Search = () => {
             <p className="search-text">
                 제목, 배우, 키워드를<br/>검색해주세요
             </p>
+        </div>
+    )
+}
+
+const SearchMovie = () => {
+    return(
+        <div className="SearchMovie">
+
         </div>
     )
 }
