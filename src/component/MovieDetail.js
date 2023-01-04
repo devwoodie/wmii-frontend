bf80850155dd29b427b5df85ed35f66a6f747e95
devwoodie <ehddl453@naver.com>
2023-01-04 16:14:33 +0900

    feat : 영화 상세정보 페이지 퍼블리싱

--- src/component/MovieDetail.js
+++ src/component/MovieDetail.js
@@ -6,6 +6,7 @@ const MovieDetail = ({apiUrl}) => {
     const location = useLocation();
     const movieId = location.state.data;
     const [infoData, setInfoData] = useState('');
+    const [infoGenres,setInfoGenres] = useState([]);
 
     useEffect(() => {
         detailInfo();
@@ -21,6 +22,7 @@ const MovieDetail = ({apiUrl}) => {
             }).then((response) => {
                 console.log(response.data)
                 setInfoData(response.data);
+                setInfoGenres(response.data.genres);
             })
         }
         catch (error){
@@ -29,8 +31,31 @@ const MovieDetail = ({apiUrl}) => {
     };
 
     return(
-        <div className="MovieDetail inner">
-
+        <div className="MovieDetail">
+            <div className="detail-wrap">
+                <div className="movie-poster-bg"><img src={"//image.tmdb.org/t/p/original/"+infoData.backdrop_path} alt="movie-bg"/></div>
+                <div className="detail-title-wrap inner">
+                    <div className="movie-poster">
+                        <img src={"//image.tmdb.org/t/p/original/"+infoData.poster_path} alt="movie-poster"/>
+                    </div>
+                    <div className="detail-text-wrap">
+                        <span className="movie-title">{infoData.title}</span>
+                        <span className="movie-title-en">{infoData.original_title}</span>
+                        <div className="sub-text-wrap">
+                            <span className="sub-text">{infoData.release_date}개봉</span>
+                            <span className="sub-text">{infoData.runtime}분</span>
+                            <span className="sub-text">
+                                {
+                                    infoGenres.map((genre,key)=>(
+                                    <i key={key}>{genre.name}</i>
+                                ))}
+                            </span>
+                            <span className="sub-text">{Math.ceil(infoData.vote_average*100)/100}점</span>
+                        </div>
+                    </div>
+                </div>
+                {/*<div>sssssss</div>*/}
+            </div>
         </div>
     )
 }
