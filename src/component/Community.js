5582057252dc3853f12dab6fff3fce36c5822132
devwoodie <ehddl453@naver.com>
2023-01-10 16:41:32 +0900

    feat : 커뮤니티 페이지 퍼블리싱

--- src/component/Community.js
+++ src/component/Community.js
@@ -1,9 +1,12 @@
 import {BiMessageSquareDetail} from "react-icons/bi";
-import {BsPlusLg} from "react-icons/bs";
+import {BsFillPencilFill, BsPlusLg} from "react-icons/bs";
+import {MdOutlineCancel} from "react-icons/md";
 import axios from "axios";
 import {useEffect, useState} from "react";
 
 const Community = () => {
+
+    const [isRegist, setIsResgist] = useState(false);
 
     return(
         <div className="Community inner">
@@ -18,16 +21,16 @@ const Community = () => {
                     </div>
                 </li>
                 <li className="community-cont">
-                    <span className="label">영화후기</span>
-                    <p className="com-text">테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트</p>
+                    <span className="label">영화추천</span>
+                    <p className="com-text">테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트</p>
                     <div className="com-sm-text">
                         <span className="com-now-date">2021-01-10</span>
                         <span className="com-user-name"><span className="com-user-nick">아니그게아니고</span> 님</span>
                     </div>
                 </li>
                 <li className="community-cont">
-                    <span className="label">영화후기</span>
-                    <p className="com-text">테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트</p>
+                    <span className="label">영화추천</span>
+                    <p className="com-text">테스트</p>
                     <div className="com-sm-text">
                         <span className="com-now-date">2021-01-10</span>
                         <span className="com-user-name"><span className="com-user-nick">아니그게아니고</span> 님</span>
@@ -50,8 +53,27 @@ const Community = () => {
                     </div>
                 </li>
             </ul>
-            <button className="regist-btn"><BsPlusLg /></button>
-            <div className="write-wrap"></div>
+            {
+                !isRegist ? <button onClick={() => {setIsResgist(true)}} type="button" className="regist-btn"><BsPlusLg /></button> :
+                    <WriteWrap setIsResgist={setIsResgist} />
+            }
+        </div>
+    )
+}
+
+const WriteWrap = ({setIsResgist}) => {
+    return(
+        <div className="write-wrap inner">
+            <span onClick={() => {setIsResgist(false)}} className="write-cls"><MdOutlineCancel/></span>
+            <div className="label-wrap">
+                <span className="label on">영화후기</span>
+                <span className="label">영화추천</span>
+                <span className="label">자유내용</span>
+            </div>
+            <div className="com-textarea">
+                <textarea></textarea>
+            </div>
+            <button type="button" className="regist-ok"><BsFillPencilFill/> 글쓰기</button>
         </div>
     )
 }
