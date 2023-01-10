4001c3f694b5a004ff0ee7541997e801836b3c1e
devwoodie <ehddl453@naver.com>
2023-01-10 13:26:02 +0900

    feat : 영화 추천 페이지 -> 커뮤니티 페이지 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './css/style.css';
 import Home from "./component/Home";
-import Recommend from "./component/Recommend";
+import Community from "./component/Community";
 import Search from "./component/Search";
 import MyInfo from "./component/MyInfo";
 import MyInfoDetail from "./component/MyInfoDetail";
 import MovieDetail from "./component/MovieDetail";
-import { MdOutlineHome, MdOutlineRecommend, MdOutlineManageSearch } from 'react-icons/md';
+import { MdOutlineHome, MdOutlineMessage, MdOutlineManageSearch } from 'react-icons/md';
 import { HiOutlineUserCircle } from 'react-icons/hi';
+import { BiMessageDetail } from 'react-icons/bi';
+
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import {useEffect, useState} from "react";
 
@@ -26,7 +28,7 @@ const App = () => {
             </div>
             <Routes>
                 <Route path="/" element={<Home apiUrl={apiUrl} navigate={navigate} setPageIndex={setPageIndex} Link={Link} />} />
-                <Route path="/recommend" element={<Recommend />} />
+                <Route path="/community" element={<Community />} />
                 <Route path="/search" element={<Search apiUrl={apiUrl} />} />
                 <Route path="/myinfo" element={<MyInfo HiOutlineUserCircle={HiOutlineUserCircle} navigate={navigate} />} />
                 <Route path="/myinfo/detail" element={<MyInfoDetail />} />
@@ -41,8 +43,8 @@ const NavTab = ({navigate,pageIndex,setPageIndex}) => {
     return(
         <div className="navi-wrap">
             <button className={pageIndex === 0 ? "navi-btn active" : "navi-btn"} onClick={() => {navigate('/'); setPageIndex(0)}}><MdOutlineHome/></button>
-            <button className={pageIndex === 1 ? "navi-btn active" : "navi-btn"} onClick={() => {navigate('/recommend'); setPageIndex(1)}}><MdOutlineRecommend /></button>
-            <button className={pageIndex === 2 ? "navi-btn active" : "navi-btn"} onClick={() => {navigate('/search'); setPageIndex(2)}}><MdOutlineManageSearch /></button>
+            <button className={pageIndex === 1 ? "navi-btn active" : "navi-btn"} onClick={() => {navigate('/search'); setPageIndex(1)}}><MdOutlineManageSearch /></button>
+            <button className={pageIndex === 2 ? "navi-btn active" : "navi-btn"} onClick={() => {navigate('/community'); setPageIndex(2)}}><BiMessageDetail /></button>
             <button className={pageIndex === 3 ? "navi-btn active" : "navi-btn"} onClick={() => {navigate('/myinfo'); setPageIndex(3)}}><HiOutlineUserCircle /></button>
         </div>
     )
---
+++ src/component/Community.js
@@ -0,0 +1,16 @@
+import {MdOutlineMovieCreation} from "react-icons/md";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+const Community = () => {
+
+    return(
+        <div className="Community inner">
+            <h2 className="wrap-tit"><span className="tit-icon"><MdOutlineMovieCreation/></span> 커뮤니티</h2>
+
+        </div>
+    )
+}
+
+
+export default Community;
